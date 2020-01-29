4b2009d31486a41259d3bf0980bec5c80e816591
Florian Stadler <flrn.stdlr@gmail.com>
2020-01-29 15:22:09 +0100

    Prepared for github pages

--- go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/flostadler/name-generator
-
-go 1.13
-
-require (
-	github.com/docker/docker v1.13.1
-	github.com/gorilla/mux v1.7.1
-)
--- pkg/randomizer/randomizer.go
+++
@@ -1,9 +0,0 @@
-package randomizer
-
-import (
-	"github.com/docker/docker/pkg/random"
-)
-
-func GetRandom(values []string) string {
-	return values[random.Rand.Intn(len(values))]
-}
