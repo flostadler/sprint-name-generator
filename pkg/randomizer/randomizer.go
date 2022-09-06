a7b288bed306ce21fcda743e040f93cd9f3e8e67
Taras Polovyi <polioviy.taras@gmail.com>
2022-09-06 14:23:15 +0300

    add randomizer seeding for unique pseudo-random numbers set

--- pkg/randomizer/randomizer.go
+++ pkg/randomizer/randomizer.go
@@ -2,7 +2,12 @@ package randomizer
 
 import (
 	"math/rand"
+	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func GetRandom(values []string) string {
 	return values[rand.Intn(len(values))]
